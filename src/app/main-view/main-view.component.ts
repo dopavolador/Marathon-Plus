78444110913d9ce5e141b0782fb836c868af1212
Gyauss <diegoacevedo.esteban@gmail.com>
2024-10-22 02:43:03 -0300

    Re font - Display flex

--- src/app/main-view/main-view.component.ts
+++ src/app/main-view/main-view.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main-view',
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './main-view.component.html',
   styleUrl: './main-view.component.css'
 })
-export class MainViewComponent {
+export class MainViewComponent{
 
 }
+
