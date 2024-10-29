f2a6b838307e8ac34f42f655593c0ebd08c1802b
Bastián Heresmann <emiliarwr@gmail.com>
2024-10-29 17:59:36 -0300

    Diagramas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,18 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { CommonModule } from '@angular/common';
 
-
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, RouterLink, RouterModule, HeaderComponent,FooterComponent],
+  imports:
+  [
+    CommonModule,
+    RouterOutlet,
+    RouterLink,
+    RouterModule,
+    HeaderComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
